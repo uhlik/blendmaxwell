5182385b06c9f023d1ac8ceb74a9bf0490cfeb8f
Jakub Uhlik <jakub.uhlik@gmail.com>
2015-04-01 13:41:38 +0200

    level up!

--- __init__.py
+++ __init__.py
@@ -19,12 +19,12 @@
 bl_info = {"name": "Maxwell Render",
            "description": "Maxwell Render integration",
            "author": "Jakub Uhlik",
-           "version": (0, 1, 4),
+           "version": (0, 1, 5),
            "blender": (2, 74, 0),
            "location": "Info header > render engine menu",
            "warning": "Currently only Mac OS X is fully supported",
-           "wiki_url": "",
-           "tracker_url": "",
+           "wiki_url": "https://github.com/uhlik/render_maxwell/wiki",
+           "tracker_url": "https://github.com/uhlik/render_maxwell/issues",
            "category": "Render", }
 
 
