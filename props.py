7390b470ec91dbd8682b193b33372577ecd7943f
Jakub Uhlik <jakub.uhlik@gmail.com>
2015-05-15 06:28:14 +0200

    pep8

--- props.py
+++ props.py
@@ -483,7 +483,7 @@ class ObjectProperties(PropertyGroup):
     backface_material_file = StringProperty(name="Backface MXM File", default="", subtype='FILE_PATH', )
     
     # hide = BoolProperty(name="Export, but Hide from Render", default=False, )
-    hide = BoolProperty(name="Export as Hidden Object", default=False, description="Object will be exported, but with visibility set to Hidden. Useful for finishing scene in Studio" )
+    hide = BoolProperty(name="Export as Hidden Object", default=False, description="Object will be exported, but with visibility set to Hidden. Useful for finishing scene in Studio")
     override_instance = BoolProperty(name="Override Instancing", default=False, )
     
     @classmethod
