96bdeb8c651a8654fee56082d334f708b12389a9
Jakub Uhlik <jakub.uhlik@gmail.com>
2015-09-18 07:21:21 +0200

    level up!

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@
 bl_info = {"name": "Maxwell Render",
            "description": "Maxwell Render integration",
            "author": "Jakub Uhlik",
-           "version": (0, 3, 0),
+           "version": (0, 3, 2),
            "blender": (2, 75, 0),
            "location": "Info header > render engine menu",
            "warning": "Alpha",
